5d0c76c0329adc3254108f407b6a07e57e3d35d8
PhilippePlagnol <philippe.plagnol@gmail.com>
2013-10-20 10:42:50 +0200

    transfer documents to product.okfn.org

--- explorer/template_download.php
+++ explorer/template_download.php
@@ -1,8 +1,7 @@
 <h1>Download</h1>
 
 Please visit first the page explaining <a href="terms-of-use">the terms of use</a><br/>
-If a link doesn't work, please send me an email (philippe.plagnol@product-open-data.com)
-<br />
+
 <p style="color:red;" > Please use Twitter to be informed of the next releases </p>
 <p>
 
@@ -10,43 +9,6 @@ If a link doesn't work, please send me an email (philippe.plagnol@product-open-d
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </p>
 
-<h2>POD Publications</h2>
-<table style="background-color: #f0eeee; ; width: 100%;" border="0" cellspacing="5" cellpadding="5" align="center">
-<tbody>
-    <tr>
-        <td>
-             <ul>
-                 <li>
-                 	2013.07.17 - <b>POD Status</b> Slides : 
-                    <a href="docs/POD-Status-2013.07.17_01.pdf" target="_blank" rel="nofollow">Direct download</a>
-                 </li>  
-                 <li>
-                 	2013.04.22 - <b>POD presented during W3C, OKFN, ODI Open data on the Web Workshop</b> Slides : 
-                   	<a href="http://www.w3.org/2013/04/odw/agenda#product" target="_blank" rel="nofollow">Link</a> 
-                    or 
-                    <a href="docs/POD-2013.04.22-W3C-OKF-ODI.pdf" target="_blank" rel="nofollow">Direct download</a>
-                 </li>  
-                 <li>
-                 	2013.03.29 - <b>POD Presentation</b> Slides : 
-                    <a href="docs/POD-2013.03.29-Presentation.pdf" target="_blank" rel="nofollow">Direct download</a>
-                 </li>
-                 <li>
-                 	2013.03.08 - <b>"Opening product data for a more responsable world"</b> Article in Open Knowledge Foundation : 
-                 	<a href="http://blog.okfn.org/2013/03/08/opening-product-data-for-a-more-responsible-world/" target="_blank" rel="nofollow">Link</a> 
-                    or 
-                    <a href="docs/POD-2013.03.08-OKF.png" target="_blank" rel="nofollow">Direct download</a>
-                 </li>
-                 <li>
-                 	2013.02.28 - <b>"Product Open Data, launches providing consumers product information in a single view"</b> Article in CivSource : 
-                    <a href="http://civsourceonline.com/2013/02/28/product-open-data-launches-providing-consumers-product-information-in-a-single-view/" target="_blank" rel="nofollow">Link</a> 
-                    or 
-                    <a href="docs/POD-2013.02.28-civsourceonline.com-Product_Open_Data_launches_providing_consumers_product_information_in_a_single_view.pdf" target="_blank" rel="nofollow">Direct download</a>
-                 </li>
-             </ul>    
-        </td>
-    </tr>
-</tbody>
-</table>
 
 <h2>POD Database</h2>
 <table style="background-color: #f0eeee; ; width: 100%;" border="0" cellspacing="5" cellpadding="5" align="center">
